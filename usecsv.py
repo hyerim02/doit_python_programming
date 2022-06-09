ccabc15566fbcf3f8811264d5dbb81369d9c2b5d
hyerim02 <phl0218@naver.com>
2022-06-10 00:33:42 +0900

    study

--- usecsv.py
+++ usecsv.py
@@ -1,15 +1,23 @@
-import csv, os
-
-def writecsv(filename,the_list):
-    with open(filename,'w',newline= '') as f:
-        a=csv.writer(f,delimiter= ',')
-        a.writerows(the_list)
+import csv, os, re
 
 def opencsv(filename):
-    f=open(filename,'r',encoding='utf-8-sig')
-    reader=csv.reader(f)
-    output=[]
+    f = open(filename, 'r')
+    reader = csv.reader(f)
+    output = []
     for i in reader:
         output.append(i)
-    f.close()    
-    return output  
+    return output
+
+def writecsv(filename, the_list):
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(the_list)
+
+def switch(listName):
+    for i in listName:
+        for j in i:
+            try:
+                i[i.index(j)] = float(re.sub(',', '', j))
+            except:
+                pass
+    return listName
